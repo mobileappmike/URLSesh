c28d534214cd69e65438686d4aabc17837c2cc5a
Michael Miles <michael.gage.miles@gmail.com>
2019-08-07 22:54:45 -0700

    Set up URLSession call for Chuck Norris API

--- URLSesh/ViewController.swift
+++ URLSesh/ViewController.swift
@@ -19,6 +19,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func chuckButtonPressed(_ sender: Any) {
+        guard let url = URL(string: "https://api.chucknorris.io/jokes/random") else {
+            fatalError("Chuck Norris URL Error")
+        }
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    if let dict = json as? [String : Any] {
+                        if let value = dict["value"] as? String {
+                            print(value)
+                        }
+                    }
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+        }.resume()
     }
     
     @IBAction func pastebinButtonPressed(_ sender: Any) {
